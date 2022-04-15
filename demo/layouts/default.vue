bcd27204769c7f67e3b5beb639da39bddda3ce61
Robert Reinhard <robert.reinhard@bukwild.com>
2022-04-15 11:47:36 -0700

    Documenting adding nuxtI18nHead

---
+++ demo/layouts/default.vue
@@ -0,0 +1,17 @@
+<!-- Default layout -->
+
+<template lang='pug'>
+
+nuxt
+
+</template>
+
+<!-- ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+<script lang='coffee'>
+export default
+
+	head: -> @$nuxtI18nHead addSeoAttributes: true
+
+</script>
+
